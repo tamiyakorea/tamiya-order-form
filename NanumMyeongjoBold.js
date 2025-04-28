11d6a08ea8272f3af82fe94fcd4e1cbee4382c2c
tamiyakorea <Jsmith14239@tamiya.co.kr>
2025-04-28 10:46:54 +0900

    Delete NanumMyeongjoBold.js

--- NanumMyeongjoBold.js
+++
@@ -1,8 +0,0 @@
-
-var NanumMyeongjoBold = "BASE64_ENCODED_FONT_DATA_BOLD_HERE";
-if (typeof jsPDF !== "undefined") {
-  jsPDF.API.events.push(["addFonts", function() {
-    this.addFileToVFS("NanumMyeongjoBold.ttf", NanumMyeongjoBold);
-    this.addFont("NanumMyeongjoBold.ttf", "NanumMyeongjo", "bold");
-  }]);
-}
