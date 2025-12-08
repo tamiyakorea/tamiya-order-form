7655c463ccb235e1bc9ab81a430a1c76caa80610
Dong-jin86 <djk@tamiya.co.kr>
2025-12-08 16:47:47 +0900

    Update orders-enhanced.js

--- admin/orders-enhanced.js
+++ admin/orders-enhanced.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.86.2/+esm';
 import * as XLSX from 'https://cdn.jsdelivr.net/npm/xlsx/+esm';
 
 const supabase = createClient(
